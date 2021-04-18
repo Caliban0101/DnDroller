54b8adebd0e42d3a959d43cd7fe183f41a729554
Robert-Colman <rcolman@gmail.com>
2021-04-19 04:42:42 +1000

    Add files via upload

---
+++ random number.py
@@ -0,0 +1,27 @@
+from random import randint
+from re import findall
+
+#dice rolling program
+#asking for input
+str = input("What dice would you like to roll? (2d4 etc). type 'quit' to close")
+
+running = True
+while running == True:
+#if quit is found in the input the program ends
+    if str.find('quit') != -1:
+        running = False
+        break
+#otherwise, numbers is a list of numbers in the string
+#lsint is that list converted to integers
+    numbers = findall(r'\d+', str)
+    lsint = [int(i) for i in numbers]
+    a = lsint[0]
+    b = randint(1,lsint[1])
+    while a > 1:
+        b = b + randint(1,lsint[1])
+        a = a - 1
+    print(b)
+    str = input("What dice would you like to roll? (2d4 etc). 'quit' to close")
+print('Bye!')
+    
+
